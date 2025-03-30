eac89c1a7a909e25003a5547e47028ef4c9a2230
Harsha-S2604 <harsha91148@outlook.com>
2025-03-30 23:37:24 +0530

    flag validation

--- main.c
+++ main.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello world\n");
+char flags[] = {'c', 'l', 'm', 'w'};
+
+int isFlagValid(char flag) {
+    int flagPresent = 0;
+    int totalFlags = sizeof(flags);
+
+    // binary search to check for valid
+    int left = 0, right = totalFlags - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        char validFlag = flags[mid];
+
+        if (validFlag == flag) {
+            flagPresent = 1;
+            break;
+        }
+
+        if (validFlag > flag) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return flagPresent;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        printf("Please provide the valid filename\n");
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            int validFlag = isFlagValid(argv[i][1]);
+
+            if (!validFlag) {
+                printf("Please provide the valid flags.\nThe Valid flags are:\n-c, -l, -m, -w\n");
+                return 0;
+            }
+        }
+        
+        
+    }
+
+    return 1;
 }
