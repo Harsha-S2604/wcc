bb60998683327985fe2a6a4878bcab13dd54dec8
Harsha-S2604 <harsha91148@outlook.com>
2025-04-14 23:02:46 +0530

    feat: implementation of byte count and line count - use fgetc to assign the 1 byte of character and count the bytes of each file - if the character is newline then increase the line count - display function to show the end result

--- main.c
+++ main.c
@@ -9,6 +9,17 @@ typedef struct flags {
     int m;
 } flags;
 
+int MAX_BYTES_STORAGE = 1024;
+
+unsigned long long int* bytes;
+unsigned long long int* lines;
+char** fileNames;
+
+FILE* getFileStream(char* fileName) {
+    FILE* fileStream = fopen(fileName, "r");
+    return fileStream;
+}
+
 flags getFlags(char* userFlags[], int totalArgs) {
     int opt;
     flags flagOps = { 0, 0, 0, 0 };
@@ -51,19 +62,107 @@ int getFileIdx (char* userFlags[], int totalArgs) {
     return fileIdx;
 }
 
+void allocateMemory(flags flagOps, int size) {
+    fileNames = (char**)realloc(fileNames, size * sizeof(char*));
+
+    if (flagOps.c) {
+        bytes = (unsigned long long int*)realloc(bytes, size * sizeof(long long int));
+    }
+
+    if (flagOps.l) {
+        lines = (unsigned long long int*)realloc(lines, size * sizeof(long long int));
+    }
+}
+
+void initDefaultValue(flags flagOps, int idx) {
+    if (flagOps.c) {
+        bytes[idx] = 0;
+    }
+
+    if (flagOps.l) {
+        lines[idx] = 0;
+    }
+}
+
+void display(flags flagOps, int totalFiles) {
+    unsigned long long int totalBytes = 0;
+    unsigned long long int totalLines = 0;
+    unsigned long long int i;
+
+    for (i = 0; i < totalFiles; i++) {
+        char* fileName = fileNames[i];
+        
+        if (flagOps.c) {
+            unsigned long long int byte = bytes[i];
+            totalBytes += byte;
+            fprintf(stdout, "%lld ", byte);
+        }
+
+        if (flagOps.l) {
+            unsigned long long int line = lines[i];
+            totalLines += line;
+            fprintf(stdout, "%lld ", line);
+        }
+
+        fprintf(stdout, "%s\n", fileName);
+    }
+
+    fprintf(stdout, "%lld %lld %s\n", totalBytes, totalLines, "total");
+}
+
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        fprintf(stderr, "please provide the valid filename\n");
+        fprintf(stderr, "[ERROR]:: Please provide the valid filename\n");
         exit(1);
     }
 
     flags flagOps = getFlags(argv, argc);
     int fileIdx = getFileIdx(argv, argc);
+    if (fileIdx == -1) {
+        fprintf(stderr, "[ERROR]:: Please provide the valid filename\n");
+        exit(1);
+    }
 
     for (int argIdx = fileIdx; argIdx < argc; argIdx++) {
         char* fileName = argv[argIdx];
-        printf("The filename is %s\n", fileName);
+
+        FILE* fileStream = getFileStream(fileName);
+        if (fileStream == NULL) {
+            fprintf(stderr, "[ERROR]:: Failed to open the file %s\n", fileName);
+            continue;
+        }
+
+        allocateMemory(flagOps, (argIdx - fileIdx) + 1);
+        initDefaultValue(flagOps, argIdx - fileIdx);
+
+        fileNames[argIdx - fileIdx] = fileName;
+
+        char character;
+        while ((character = fgetc(fileStream)) != EOF) {
+            if (flagOps.c) {
+                bytes[argIdx - fileIdx]++;
+            } else {
+                bytes = NULL;
+            }
+
+            if (flagOps.l) {
+                if (character == '\n') {
+                    lines[argIdx - fileIdx]++;
+                }
+            } else {
+                lines = NULL;
+            }
+        }
+
+        fclose(fileStream);
     }
+
+    int totalFiles = argc - fileIdx;
+    display(flagOps, totalFiles);
+
+    free(bytes);
+    free(lines);
+    free(fileNames);
 
     return 0;
 }
